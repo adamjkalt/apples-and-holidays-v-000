bb51ce7fb149dce2aa76e4954a41a3b8e5b30896
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-21 03:31:38 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supplies|
       if supplies.include?("BBQ")
-        holiday
+        return holiday
       end
       end
     end
