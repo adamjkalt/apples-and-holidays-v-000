c632027fcb697f2cdf20ab297eb39e6b74bbb2d2
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-21 03:35:43 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,12 +71,12 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.each do |season, holidays|
+  end
     holidays.each do |holiday, supplies|
       if supplies.include?("BBQ")
-        return holiday
+        holiday
       else
         false
-      end
       end
     end
 end
